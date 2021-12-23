d20afd6b5857263f236df6d9548473363fc0f102
dan luo <danluo@dandeMacBook-Air.local>
2021-12-23 17:18:39 +0800

    1

--- bin/setup_env.sh
+++ bin/setup_env.sh
@@ -16,7 +16,7 @@ then
 fi
 
 echo "${LOG_PREFIX}start to sync genesis header side chain"
-${POLY_HOME}/lib/tools/side_chain_mgr -tool sync_genesis_header -conf ${POLY_HOME}/lib/tools/config.json -pwallets "${POLY_HOME}/lib/poly/wallet.dat,${POLY_HOME}/lib/poly/wallet1.dat,${POLY_HOME}/lib/poly/wallet2.dat,${POLY_HOME}/lib/poly/wallet3.dat" -ppwds "4cUYqGj2yib718E7ZmGQc,4cUYqGj2yib718E7ZmGQc,4cUYqGj2yib718E7ZmGQc,4cUYqGj2yib718E7ZmGQc" -owallets "${POLY_HOME}/lib/ontology/wallet.dat,${POLY_HOME}/lib/ontology/wallet1.dat,${POLY_HOME}/lib/ontology/wallet2.dat,${POLY_HOME}/lib/ontology/wallet3.dat,${POLY_HOME}/lib/ontology/wallet4.dat,${POLY_HOME}/lib/ontology/wallet5.dat,${POLY_HOME}/lib/ontology/wallet6.dat" -opwds "admin,123,123,123,123,123,123"
+${POLY_HOME}/lib/tools/side_chain_mgr -tool sync_genesis_header -chainid 2 -conf ${POLY_HOME}/lib/tools/config.json -pwallets "${POLY_HOME}/lib/poly/wallet.dat,${POLY_HOME}/lib/poly/wallet1.dat,${POLY_HOME}/lib/poly/wallet2.dat,${POLY_HOME}/lib/poly/wallet3.dat" -ppwds "4cUYqGj2yib718E7ZmGQc,4cUYqGj2yib718E7ZmGQc,4cUYqGj2yib718E7ZmGQc,4cUYqGj2yib718E7ZmGQc" 
 if [ $? -ne 0 ]
 then
     echo "${LOG_PREFIX}failed to sync genesis header"
