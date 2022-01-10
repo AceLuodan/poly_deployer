33eeb1827f4554fc4b02e7bf8886a8667a9981ff
AceLuodan <903585177@qq.com>
2022-01-10 16:12:17 +0800

    Update setup_env.sh

--- bin/setup_env.sh
+++ bin/setup_env.sh
@@ -31,14 +31,6 @@ mv ${POLY_HOME}/lib/relayer_eth/config.json1 ${POLY_HOME}/lib/relayer_eth/config
 
 
 
-echo "${LOG_PREFIX}start to register fabric side chain"
-${POLY_HOME}/lib/tools/side_chain_mgr -tool register_side_chain  -chainid 7  -conf ${POLY_HOME}/lib/tools/config.json -pwallets "${POLY_HOME}/lib/poly/wallet1.dat,${POLY_HOME}/lib/poly/wallet2.dat,${POLY_HOME}/lib/poly/wallet3.dat" -ppwds "4cUYqGj2yib718E7ZmGQc,4cUYqGj2yib718E7ZmGQc,4cUYqGj2yib718E7ZmGQc"
-if [ $? -ne 0 ]
-then
-    echo "${LOG_PREFIX}failed to register_side_chain"
-    exit 1
-fi
-
 # echo "${LOG_PREFIX}init ont account"
 # while true 
 # do
